170d09dd20cfb82d012eb5ab419d12d4c15a00ae
JC306010 <116584138+JC306010@users.noreply.github.com>
2022-11-22 12:43:23 -0600

    added code to prank

---
+++ Prank.js
@@ -0,0 +1,4 @@
+console.log('Hello World');
+let a = createElement (onclick, 'button', '100% Free Gator Music');
+let = function onclick(_alert('HOW DARE YOU TRY TO STEAL GATORS MUSIC'));
+document.body.appendChild(a);
